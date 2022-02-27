9165aa2336f5da36af7de60e48ee6b82d44a0237
Serega S <serega1067@yandex.ru>
2022-02-27 18:15:38 +0700

    Homework seminar1

---
+++ task2/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 2: Напишите программу, которая 
+// принимает на вход три числа и выдаёт 
+// максимальное из этих чисел.
+
+Console.WriteLine("Узнаем максимальное число из " +
+                  "трёх чисел.\nВведите первое число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int num_temp = Convert.ToInt32(Console.ReadLine());
+
+if (num < num_temp)
+{
+    num = num_temp;
+}
+Console.WriteLine("Введите трутье число: ");
+num_temp = Convert.ToInt32(Console.ReadLine());
+
+if (num < num_temp)
+{
+    num = num_temp;
+}
+Console.WriteLine("Максимальное число: " + num);
---
+++ task4/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 4: Напишите программу, которая на вход 
+// принимает число (N), а на выходе показывает 
+// все чётные числа от 1 до N.
+
+Console.WriteLine("Введите число: ");
+int lenList = Convert.ToInt32(Console.ReadLine());
+int num = 1;
+
+while (num <= lenList)
+{
+    if (num % 2 == 0)
+    {
+        Console.WriteLine(num);
+    }
+    num++;
+}
